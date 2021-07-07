0ffb964fa654c7fb581cccdf597cd11c66d9da7a
Masatoshi <msts.oo0131@gmail.com>
2021-07-07 20:26:26 +0900

    delete_method_create

--- lesson/delete-lesson.php
+++ lesson/delete-lesson.php
@@ -2,20 +2,21 @@
 include('../account/partials/header.php');
 
 
-$id= $_GET['id'];
+$lesson_id= $_GET['lesson_id'];
 
-$sql= "DELETE FROM tbl_account WHERE id=$id";
-$rec= mysqli_query($connect, $sql);
+$sql2= "DELETE FROM tbl_lesson WHERE lesson_id=$lesson_id";
+$rec2= mysqli_query($connect, $sql2);
 
-if($rec == TRUE) {
+if($rec2 == TRUE) {
+
     $_SESSION['delete'] = "<div class='success'>Delete Lesson Successfully.</div>";
-    $url = "http://localhost:8001/account/login.php";
+    $url = "http://localhost:8001/lesson/manage-lesson.php?account_id=$account_id";
     header('Location:' .$url,true , 302);
 }
 else
 {
     $_SESSION['delete'] = "<div class='error'>Failed to Delete lesson.</div>";
-    $url = "http://localhost:8001/account/manage-client.php?id=$id";
+    $url = "http://localhost:8001/lesson/manage-client.php?id=$account_id";
     header('Location:' .$url,true , 401);//ページへのリダイレクトをif~else文にて行っている。
 }
 
