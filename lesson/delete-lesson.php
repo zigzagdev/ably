9d93f97f7544d09217ec23d4011e54fe937a09e7
Masatoshi <msts.oo0131@gmail.com>
2021-07-07 20:22:55 +0900

    delete_lesson_base_pull

--- lesson/delete-lesson.php
+++ lesson/delete-lesson.php
@@ -1,1 +1,23 @@
 <?php
+include('../account/partials/header.php');
+
+
+$id= $_GET['id'];
+
+$sql= "DELETE FROM tbl_account WHERE id=$id";
+$rec= mysqli_query($connect, $sql);
+
+if($rec == TRUE) {
+    $_SESSION['delete'] = "<div class='success'>Delete Lesson Successfully.</div>";
+    $url = "http://localhost:8001/account/login.php";
+    header('Location:' .$url,true , 302);
+}
+else
+{
+    $_SESSION['delete'] = "<div class='error'>Failed to Delete lesson.</div>";
+    $url = "http://localhost:8001/account/manage-client.php?id=$id";
+    header('Location:' .$url,true , 401);//ページへのリダイレクトをif~else文にて行っている。
+}
+
+include('../account/partials/footer.php');
+?>
