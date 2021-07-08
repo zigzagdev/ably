b693d6fb45d0d5e011794d96e5237eec02a35688
Masatoshi <msts.oo0131@gmail.com>
2021-07-08 18:20:23 +0900

    lesson_base_create

--- account/index.php
+++ account/index.php
@@ -1,9 +1,9 @@
-<?php include('partials/header.php'); ?>
+<?php include('partials/header_info.php'); ?>
 
 <!--Main Section -->
 <div class="main">
     <div class="wrapper">
-        <h1 style="padding: 20px 0 0 50px">This is your account</h1>
+        <h1 style="padding: 20px 0 0 50px">Lesson Index</h1>
         <?php
         if(isset($_SESSION['login']))
         {
@@ -11,20 +11,51 @@
             unset($_SESSION['login']);
         }
         ?>
-        <br/><br/>
 
-        <div class="col-4 text-center">
+        <br/><br/><br/>
+        <table class="tbl-full">
+            <tr>
+                <th>Course</th>
+                <th>Content</th>
+                <th>Lesson Day</th>
+            </tr>
             <?php
-            $sql="SELECT * FROM tbl_account";
-            $rec=mysqli_query($connect,$sql);
-            $count=mysqli_num_rows($rec);
+            $sql2 = "SELECT * FROM tbl_lesson ";
+            $rec2 = mysqli_query($connect, $sql2);
+
+
+            if($rec2==TRUE)
+            {
+                $count = mysqli_num_rows($rec2); // Function to get all the rows in database
+
+                $on=1;
+
+                if($count>0)
+                {
+                    while($rows=mysqli_fetch_assoc($rec2))
+                    {
+                        $course = $rows['course'];
+                        $content = $rows['content'];
+                        $day = $rows['day'];
+                        $lesson_id = $rows['lesson_id'];
+                        ?>
+                        <tr>
+                            <td><?php echo $course; ?></td>
+                            <td><?php echo $content; ?></td>
+                            <td><?php echo $day; ?></td>
+                        </tr>
+                        <?php
+                    }
+                }
+                else
+                {
+                    //
+                }
+            }
             ?>
-            <h1><?php echo $count; ?></h1>
-            <br/>
-            Participations
-        </div>
-
-        <div class="clearfix"></div>
+        </table>
     </div>
+    <div class="clearfix"></div>
+</div>
 
-    <?php include('partials/footer.php');  ?>
+<?php include('partials/footer.php');  ?>
