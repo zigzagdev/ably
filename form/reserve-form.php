fb534e086c73ba1d04cdfba3b9053675fdbe61c4
Masatoshi <msts.oo0131@gmail.com>
2021-07-12 19:46:45 +0900

    form_submitted

--- form/reserve-form.php
+++ form/reserve-form.php
@@ -3,23 +3,21 @@
 <section class="food-search">
     <div class="container2">
         <h2 class="text-center">Fill this form to confirm your order.</h2><br/><br/>
-        <form action="" method="POST" class="order" style="text-align: center" >
+        <form action="reserve-form.php" method="POST" class="order" style="text-align: center" >
             <fieldset class="fieldset">
                 <legend class="legend-center">Delivery Details</legend>
                 <div class="order-label text-white" >Full Name</div>
-                <input type="text"  name="full-name" placeholder="Test Test" class="input-responsive" required>
+                <input type="text"  name="name" placeholder="Test" class="input-responsive" required>
                 <div class="order-label text-white">Phone Number</div>
-                <input type="tel" name="contact" placeholder="090-1234-1234" class="input-responsive" required>
+                <input type="telephone" name="telephone" placeholder="090-1234-1234" class="input-responsive" required>
                 <div class="order-label ">Email</div>
-                <input type="email" name="email" placeholder="1234aa@test.com" class="input-responsive" required>
-                <div class="order-label text-white">Sex</div>
-                <select name="sex" class="input-responsive" required>
-                    <option value="">Choose here.</option>
-                    <option value="選択肢1">Male</option>
-                    <option value="選択肢2">Female</option>
-                </select></fieldset><br/>
-            <input type="submit" name="submit" value="Confirm Order" class="btn btn-primary">
+                <input type="email" name="email" placeholder="1234aa@test.com" class="input-responsive" required><br/><br/>
+                <select name= "sex">
+                    <option value = "male">Male</option>
+                    <option value = "female">Female</option>
+                </select><br/>
             </fieldset>
+            <input type="submit" name="submit" value="送信" class="btn btn-third">
         </form>
     </div>
 </section>
@@ -30,18 +28,27 @@ if(isset($_POST['submit']))
 {
     $name = $_POST['name'];
     $telephone = $_POST['telephone'];
-    $email = $_POST['email'];
-    $email2 = '/¥A\w\-\.]+¥@[\w\-\.]+.([a-z]+)\z/';
-    if(preg_match($email,$email2))
+    $tel_boolean="/^(([0-9]{3}-[0-9]{4})|([0-9]{7}))$/";
+    if(preg_match($tel_boolean,$telephone))
     {
-        print 'write down your email correctly ! ';
+        print  'write down your phone number correctly !';
     }
     else
     {
         //
     }
-    $sex = $_POST['sex'];
+    $email = $_POST['email'];
+    $contents_mail='/¥A\w\-\.]+¥@[\w\-\.]+.([a-z]+)\z/';
+    if(preg_match($contents_mail,$email))
+    {
+        print 'write down your email correctly ! ';
+    }
+    else
+    {
+            //
+    }
 
+    $sex = $_POST['sex'];
 
     $sql3 = "INSERT INTO tbl_form SET name = '$name',telephone = '$telephone',email = '$email',sex = '$sex' ";
 
@@ -62,7 +69,4 @@ if(isset($_POST['submit']))
 }
 ?>
 
-
-
-?>
 <?php include('../account/partials/footer.php'); ?>
