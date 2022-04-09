ed98b891109e09d5991e0e370cf129f6ef92ac85
zigzagdev <msts.oo0131@gmail.com>
2022-04-09 13:50:59 +0900

    CardStyle_change

--- Index.php
+++ Index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="../css/Forms.css">
   </head>
   <body>
-    <div style="margin: 0 240px 0 240px">
+    <div style="margin: 0 100px 0 100px">
       <div>
        <h1 style="padding: 20px ; text-align:center">Upcoming Lessons</h1>
         <?php
@@ -29,7 +29,10 @@
         ?>
           <div class="cardoutline">
             <div class="cardcontent">
-
+              <span style="margin-top: "><?php echo $course ?></span><br/>
+              <?php echo $content ?><br/>
+              <?php echo $day ?><br/>
+              <a href="./form/ReserveForm.php?lesson_id=<?= $lesson_id?>"> Reserve your form</a>
             </div>
           </div>
         <?php
