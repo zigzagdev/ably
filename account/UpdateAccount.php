9519489a3ecf49c1c7a415c054acb55c68557fc0
zigzagdev <msts.oo0131@gmail.com>
2022-04-10 21:41:07 +0900

    UpdateAccount_CSS_input_fix

--- account/UpdateAccount.php
+++ account/UpdateAccount.php
@@ -32,72 +32,75 @@
     <link rel="stylesheet" href="../css/Forms.css">
   </head>
   <body>
-    <div class="main">
-      <div style="margin: 0 230px">
-        <h1 style="text-align: center">Update your Account</h1>
-        <form action="" method="post">
-          <input type="text" name="username" value="<?php echo $username; ?>">
-          <input type="submit" name="submit" value="Update your Account" class="btn-secondary">
-        </form>
+    <div style="margin: 0 230px">
+      <div class="mainaccount">
+        <h1 style="text-align: center; margin: 55px 0 40px 0; padding-top: 20px">Update your Account</h1>
+          <form action="" method="post" enctype="multipart/form-data">
+            <b style="font-size: 20px; margin-left: 50px">UserName</b>
+            <input type="text" name="username" class="updateform" value="<?php echo $username; ?>">
+            <hr color="#a9a9a9" width="100%" size="1" style="text-align: center;">
+            <b style="font-size: 20px; margin-left: 50px">Email</b>
+            <input type="text" name="username" class="updateform" style="margin-left: 255px" value="<?php echo $username; ?>">
+            <hr color="#a9a9a9" width="100%" size="1" style="text-align: center;">
+            <b style="font-size: 20px; margin-left: 50px">Content</b>
+            <input type="text" name="username" class="updateform" style="margin-left: 227px" value="<?php echo $username; ?>">
+            <hr color="#a9a9a9" width="100%" size="1" style="text-align: center;">
+<!--            <input type="submit" name="submit" value="Update your Account" class="btn-secondary">-->
+
+          </form>
       </div>
     </div>
   </body>
 <?php
-if(isset($_POST['submit']))
-{
+          if(isset($_POST['submit']))
+          {
+            $account_id = $_GET['account_id'];
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            $content = $_POST['content'];
 
-    $account_id = $_POST['account_id'];
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $content = $_POST['content'];
-
-    if(isset($_FILES['image']['name']))
-    {
-        $image = $_FILES['image']['name'];
-
-        if($image != "")
-        {
-
-            $source_path = $_FILES['image']['tmp_name'];
-            $destination_path = "../images/profile/".$image;
-            $upload = move_uploaded_file($source_path, $destination_path);
-
-            if($upload==false)
+          if(isset($_FILES['image']['name']))
+          {
+            $image = $_FILES['image']['name'];
+            if($image != "")
             {
+              $source_path = $_FILES['image']['tmp_name'];
+              $destination_path = "../images/profile/".$image;
+              $upload = move_uploaded_file($source_path, $destination_path);
+              if($upload!=true)
+              {
                 $_SESSION['upload'] = "<div class='error'>Failed to Upload Image. </div>";
-                $url = "http://localhost:8001/account/UpdateAccount.php?account_id=$row[account_id]";
+                $url = "http://localhost:8001/account/UpdateAccount.php?account_id=$account_id";
                 header('Location:'.$url,true , 401);
                 die();
-            }
-            if($current_image!="")
-            {
+              }
+              if($current_image!="")
+              {
                 $remove_path = "../images/profile/".$current_image;
                 $remove = unlink($remove_path);
                 if($remove==false)
                 {
-                    $_SESSION['failed-remove'] = "<div class='error'>Failed to remove current Image.</div>";
-                    $url = "http://localhost:8001/account/UpdateAccount.php?account_id=$account_id";
-                    header('Location:' .$url,true , 401);
-                    die();
+                  $_SESSION['failed-remove'] = "<div class='error'>Failed to remove current Image.</div>";
+                  $url = "http://localhost:8001/account/UpdateAccount.php?account_id=$account_id";
+                  header('Location:' .$url,true , 401);
+                  die();
                 }
+              }
+            } else
+            {
+              $image = $current_image;
             }
-        }
-        else
-        {
+          } else
+          {
             $image = $current_image;
-        }
-    }
-    else
-    {
-        $image = $current_image;
-    }
-    $sql = "UPDATE tbl_account SET 
-                   username='$username'
-                   ,image_name='$image'
-                   ,email='$email'
-                   ,content='$content' 
-            WHERE account_id=$account_id ";
-    $rec = mysqli_query($connect, $sql);
+          }
+          $sql = "UPDATE tbl_account SET 
+                         username='$username'
+                         ,image_name='$image'
+                         ,email='$email'
+                         ,content='$content' 
+                  WHERE account_id=$account_id ";
+          $rec = mysqli_query($connect, $sql);
 }
 ?>
 <?php include('partials/Footer.tpl'); ?>
