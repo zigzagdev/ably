3010c323bd52f1d53cab51f1b2d8b273010d120e
zigzagdev <msts.oo0131@gmail.com>
2022-04-10 12:39:54 +0900

    UpdateAccount_fixing

--- account/UpdateAccount.php
+++ account/UpdateAccount.php
@@ -33,70 +33,13 @@
   </head>
   <body>
     <div class="main">
-      <div>
-        <div>
-          <h1>Update your Account</h1>
-          <form action="" method="post" enctype="multipart/form-data">
-                    <table class="tbl-30">
-                        <tr>
-                            <td class="text-white">Username:</td>
-                            <td>
-                                <input type="text" name="username" value="<?php echo $username; ?>">
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td >Current Image: </td>
-                            <td>
-                                <?php
-                                if($image == "")
-                                {
-                                    echo "<div class='error'>Image not Available.</div>";
-
-                                }
-                                else
-                                {
-                                    ?>
-                                    <img src="../images/profile/<?php echo $image; ?>" width="150px">
-                                    <?php
-                                }
-                                ?>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td> Select New Image: </td>
-                            <td>
-                                <input type="file" name="image">
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td style="text-align: center">Email: </td>
-                            <td>
-                                <input name="email" name="name" value="<?php echo $email; ?>">
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td style="text-align: center">Content: </td>
-                            <td>
-                                <textarea name="content" cols="30" rows="6" ><?php echo $content; ?></textarea>
-                            </td>
-                        </tr>
-
-
-                        <tr>
-                            <td colspan="2">
-                                <input type="hidden" name="account_id" value="<?php echo $account_id; ?>">
-                                <input type="submit" name="submit" value="Update your Account" class="btn-secondary">
-                            </td>
-                        </tr>
-
-                    </table>
-                </form>
-            </div>
-        </div>
+      <div style="margin: 0 230px">
+        <h1 style="text-align: center">Update your Account</h1>
+        <form action="" method="post">
+          <input type="text" name="username" value="<?php echo $username; ?>">
+          <input type="submit" name="submit" value="Update your Account" class="btn-secondary">
+        </form>
+      </div>
     </div>
   </body>
 <?php
@@ -148,23 +91,13 @@ if(isset($_POST['submit']))
     {
         $image = $current_image;
     }
-    $sql = "UPDATE tbl_account SET username='$username',image_name='$image',email='$email',
-             content='$content' WHERE account_id=$account_id ";
+    $sql = "UPDATE tbl_account SET 
+                   username='$username'
+                   ,image_name='$image'
+                   ,email='$email'
+                   ,content='$content' 
+            WHERE account_id=$account_id ";
     $rec = mysqli_query($connect, $sql);
-
-    if($rec==true)
-    {
-        $url = "http://localhost:8001/account/ManageAccount.php?account_id=$account_id";
-        $_SESSION['update'] = "<div class='success'>Account Updated Successfully.</div>";
-        header('Location:' .$url,true , 302);
-    }
-    else
-    {
-        $_SESSION['update'] = "<div class='error'>Failed to Update Account.</div>";
-        $url = "http://localhost:8001/account/UpdateAccount.php?account_id=$account_id";
-        header('Location:' .$url,true , 401);
-        die();
-    }
 }
 ?>
 <?php include('partials/Footer.tpl'); ?>
