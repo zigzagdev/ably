515a6bbf90415f703325012aff1599542588e750
zigzagdev <msts.oo0131@gmail.com>
2022-04-28 00:58:17 +0900

    fix_eachlesson

--- lesson/EachLesson.php
+++ lesson/EachLesson.php
@@ -6,6 +6,26 @@ include "./header/LessonHeader.blade.php";
     echo $_SESSION['fail_lesson'];
     unset($_SESSION['fail_lesson']);
   }
+  $lesson_id = $_GET['lesson_id'];
+  $sql = "SELECT * FROM `tbl_lesson`";
+
+  $rec = mysqli_query($connect, $sql);
+
+  if($rec == TRUE)
+  {
+    $count = mysqli_num_rows($rec);
+    if ($count >= 0)
+    {
+      while ($rows = mysqli_fetch_array($rec))
+      {
+        $course     = $rows['course'];
+        $content    = $rows['content'];
+        $deadline   = $rows['deadline'];
+        $account_id = $rows['account_id'];
+        $created_at = $rows['created_at'];
+      }
+    }
+  }
 
 ?>
 
@@ -16,12 +36,44 @@ include "./header/LessonHeader.blade.php";
     <link rel="stylesheet" href="../css/Forms.css">
   </head>
   <body>
-
-    <div style="margin:60px 0; text-align: center">
-      <div style="margin: 0 10px 20px 10px">
-        <a class="lesson-btn-delete" style="margin: 0 7px 0 7px;" href="DeleteLesson.php?lesson_id=<?= $lesson_id=$_GET['lesson_id']?>">
-          Delete your Lesson
-        </a>
+    <div style="margin: 0 180px">
+      <div class="mainaccount">
+        <li style="list-style: none;  margin:27px 0 7px 70px; padding-top: 20px">
+          <b style="font-size: 20px;width:70px;margin-left:150px; vertical-align: 70%; ">Lesson Detail</b>
+        </li>
+        <li style="list-style: none;  margin:27px 0 17px 30px">
+          <b style="font-size: 20px;width:100px;margin-right:160px; float: left;">
+            CourseName
+          </b>
+          <b style="font-size: 20px; margin-right: 10px"><?php echo $course ?></b>
+        </li>
+        <hr color="#a9a9a9" width="100%" size="1" style="text-align: center;">
+        <li style="list-style: none;  margin:17px 0 17px 30px">
+          <b style="font-size: 20px;width:100px;margin-right:160px; float: left;">
+            Content
+          </b>
+          <b style="font-size: 20px; margin-right: 170px"><?php echo $content ?></b>
+        </li>
+        <hr color="#a9a9a9" width="100%" size="1" style="text-align: center;">
+        <li style="list-style: none;  margin:17px 0 17px 30px">
+          <b style="font-size: 20px;width:100px;margin-right:160px; float: left;">
+            Deadline
+          </b>
+          <b style="font-size: 20px; margin-right: 170px"><?php echo $deadline ?></b>
+        </li>
+      </div>
+      <div style="margin:60px 0; text-align: center">
+        <div style="margin: 0 10px 20px 10px">
+          <a class="btn-primary" style="margin: 0 7px 0 7px">
+            Update your Account
+          </a>
+          <a class="btn-secondary" style="margin: 0 7px 0 7px">
+            Check your register Lessons.
+          </a>
+          <a class="btn-delete" style="margin: 0 7px 0 7px;">
+            Delete your Account
+          </a>
+        </div>
       </div>
     </div>
   </body>
