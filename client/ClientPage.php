ad7d68fbf13f4c0dff7b93d57d04e88d6ff3d326
zigzagdev <msts.oo0131@gmail.com>
2022-05-01 22:39:15 +0900

    fix_clientpage_HTML

--- client/ClientPage.php
+++ client/ClientPage.php
@@ -36,7 +36,59 @@ include  "./partials/HeaderEd.tpl";
     }
   }
 ?>
-
+<html>
+  <head>
+    <title>ManageAccount</title>
+    <link rel="stylesheet" href="../css/Account.css">
+    <link rel="stylesheet" href="../css/Forms.css">
+  </head>
+  <body>
+    <div style="margin: 0 190px">
+      <div class="mainaccount">
+        <li style="list-style: none;  margin:27px 0 7px 70px; padding-top: 20px">
+          <a href="./UpdateImage.blade.php?client_id=<?=$client_id=$_GET['client_id']?>"
+            <img src="../images/profile/<?php echo $image; ?>" width="90px" height="90px" style="border-radius: 50%; margin-right: 160px; vertical-align: center">
+            <b style="font-size: 20px;width:70px;margin-right:10px; vertical-align: 70%">Manage your Account</b>
+          </a>
+        </li>
+        <li style="list-style: none;  margin:47px 0 17px 30px">
+          <b style="font-size: 20px;width:100px;margin-right:200px; float: left;">
+            UserName
+          </b>
+          <b style="font-size: 20px"><?php echo $name ?></b>
+        </li>
+        <hr color="#a9a9a9" width="100%" size="1" style="text-align: center;">
+        <li style="list-style: none;  margin:17px 0 17px 30px">
+          <b style="font-size: 20px;width:100px;margin-right:200px; float: left;">
+            Email
+          </b>
+          <b style="font-size: 20px"><?php echo $email ?></b>
+        </li>
+        <hr color="#a9a9a9" width="100%" size="1" style="text-align: center;">
+        <li style="list-style: none;  margin:17px 0 17px 30px">
+          <b style="font-size: 20px;width:100px;margin-right:200px; float: left;">
+            Content
+          </b>
+          <b style="font-size: 20px"><?php echo mb_strimwidth( strip_tags( $content ), 0, 20, '…', 'UTF-8' ); ?></b>
+        </li>
+        <hr color="#a9a9a9" width="100%" size="1" style="text-align: center;">
+      </div>
+    </div>
+    <div style="margin:60px 0; text-align: center">
+      <div style="margin: 0 10px 20px 10px">
+<!--        <a class="btn-primary" style="margin: 0 7px 0 7px" href="UpdateAccount.php?account_id=--><?//= $account_id=$_GET['account_id']?><!--">-->
+<!--          Update your Account-->
+<!--        </a>-->
+        <a class="btn-secondary" style="margin: 0 7px 0 7px" href="./form/ManageForm.php?client_id=<?= $client_id=$_GET['client_id']?>">
+          Check your register forms.
+        </a>
+        <a class="btn-delete" style="margin: 0 7px 0 7px;" href="DeleteClient.php?client_id=<?=$client_id=$_GET['client_id']?>">
+          Delete your Account
+        </a>
+      </div>
+    </div>
+  </body>
+</html>
 
 
 
