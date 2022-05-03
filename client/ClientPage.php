8e9757b4125fc3aea279b168d8f160d84cea86a6
zigzagdev <msts.oo0131@gmail.com>
2022-05-03 09:09:57 +0900

    client_css_fix

--- client/ClientPage.php
+++ client/ClientPage.php
@@ -28,6 +28,7 @@ include "./partials/HeaderEd.blade.php";
         $image       = $rows['image'];
         $email       = $rows['email'];
         $sex         = $rows['sex'];
+        $phone       = $rows['telephone'];
         if ($image == "")
         {
           echo "<div class='error'>Image not Added.</div>";
@@ -45,13 +46,16 @@ include "./partials/HeaderEd.blade.php";
   <body>
     <div style="margin: 0 140px">
       <div class="mainaccount" style="background-color: lightgray">
-        <li style="list-style: none;  margin:27px 0 7px 70px; padding-top: 20px">
-          <a href="./UpdateImage.blade.php?client_id=<?=$client_id=$_GET['client_id']?>" style="text-decoration: none">
-            <img src="../images/profile/<?php echo $image; ?>" width="90px" height="90px" style="border-radius: 50%; margin-right: 160px; vertical-align: center">
-            <b style="font-size: 20px;width:70px;margin-right:10px; vertical-align: 70%" class="client_update">Manage your Account</b>
+        <li style="list-style: none;  margin:47px 10px 7px 30px; padding-top: 20px">
+          <a href="./UpdateImage.blade.php?client_id=<?=$client_id=$_GET['client_id']?>" style="text-decoration: none;">
+            <img src="../images/profile/<?php echo $image; ?>" width="90px" height="90px" class="c_img">
+            <b style="font-size: 20px;width:70px;margin-right:10px; vertical-align: 70%" class="client_update">
+              Manage your Account
+            </b>
+            <p style="margin-left: 30px; line-height: 1px">Edit</p>
           </a>
         </li>
-        <li style="list-style: none;  margin:47px 0 17px 30px">
+        <li style="list-style: none;  margin:37px 0 17px 30px">
           <b style="font-size: 20px;width:100px;margin-right:200px; float: left;">
             UserName
           </b>
@@ -69,6 +73,18 @@ include "./partials/HeaderEd.blade.php";
           </b>
           <b style="font-size: 20px"><?php echo $email ?></b>
           <a href="./UpdateEmail.blade.php?client_id=<?=$client_id=$_GET['client_id']?>" style="text-decoration: none">
+            <b style="float: right; margin-right: 40px;">
+              <img src="../images/pencil.png" class="pencil">
+            </b>
+          </a>
+        </li>
+        <hr color="#a9a9a9" width="100%" size="1" style="text-align: center;">
+        <li style="list-style: none;  margin:17px 0 17px 30px">
+          <b style="font-size: 20px;width:100px;margin-right:200px; float: left;">
+            PhoneNumber
+          </b>
+          <b style="font-size: 20px"><?php echo $phone ?></b>
+          <a href="./UpdatePhoneNumber.blade.php?client_id=<?=$client_id=$_GET['client_id']?>" style="text-decoration: none">
             <b style="float: right; margin-right: 40px;">
               <img src="../images/pencil.png" class="pencil">
             </b>
