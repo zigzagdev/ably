0b79d0e0efc6c85cc2e99ec8885c505c26620104
zigzagdev <msts.oo0131@gmail.com>
2022-05-08 12:51:31 +0900

    indent_FOrmHeader_fix

--- client/partials/FormHeader.blade.php
+++ client/partials/FormHeader.blade.php
@@ -4,21 +4,21 @@ $client_id = $_GET['client_id'];
 ?>
 
 <html>
-<head>
-  <link rel="stylesheet" href="../../css/Account.css">
-</head>
-<body style="background-color: ghostwhite">
-<div class="infooutline">
-  <div class="logo">
-    <h1 class="hchar" style="color: #125EAE">Learn at here..!</h1>
-  </div>
-  <div class="account text-center">
-    <div class="wrapper">
-      <a href = "../SLogout.blade.php" style="text-decoration: none; color: black" class="wrapper-inner">Logout</a>
-      <a href = "../../LIndex.blade.php?client_id=<?= $client_id?>" style="text-decoration: none; color: black" class="wrapper-inner">Index</a>
-      <a href = "../UpPassCli.blade.php?client_id=<?= $client_id?>" style="text-decoration: none; color: black" class="wrapper-inner">PasswordChange</a>
+  <head>
+    <link rel="stylesheet" href="../../css/Account.css">
+  </head>
+  <body style="background-color: ghostwhite">
+    <div class="infooutline">
+      <div class="logo">
+        <h1 class="hchar" style="color: #125EAE">Learn at here..!</h1>
+      </div>
+      <div class="account text-center">
+        <div class="wrapper">
+          <a href = "../SLogout.blade.php" style="text-decoration: none; color: black" class="wrapper-inner">Logout</a>
+          <a href = "../../LIndex.blade.php?client_id=<?= $client_id?>" style="text-decoration: none; color: black" class="wrapper-inner">Index</a>
+          <a href = "../UpPassCli.blade.php?client_id=<?= $client_id?>" style="text-decoration: none; color: black" class="wrapper-inner">PasswordChange</a>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-</body>
+  </body>
 </html>
