92a4967402f8558682f64309344a17a4a0f88a4b
zigzagdev <msts.oo0131@gmail.com>
2022-05-08 19:40:45 +0900

    Header_fix

--- client/partials/FormHeader.blade.php
+++ client/partials/FormHeader.blade.php
@@ -15,7 +15,7 @@ $client_id = $_GET['client_id'];
       <div class="account text-center">
         <div class="wrapper">
           <a href = "../SLogout.blade.php" style="text-decoration: none; color: black" class="wrapper-inner">Logout</a>
-          <a href = "../../LIndex.blade.php?client_id=<?= $client_id?>" style="text-decoration: none; color: black" class="wrapper-inner">Index</a>
+          <a href = "../../LIndex.blade.php?client_id=<?= $client_id?>" style="text-decoration: none; color: black" class="wrapper-inner">TopPage</a>
           <a href = "../UpPassCli.blade.php?client_id=<?= $client_id?>" style="text-decoration: none; color: black" class="wrapper-inner">PasswordChange</a>
         </div>
       </div>
