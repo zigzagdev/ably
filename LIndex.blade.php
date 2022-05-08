dcfa060000561f0ed7c61ccef29631d4cbd9fa8c
zigzagdev <msts.oo0131@gmail.com>
2022-05-08 20:00:52 +0900

    cardframe_margin_fix

--- LIndex.blade.php
+++ LIndex.blade.php
@@ -9,7 +9,7 @@ include('./account/partials/ClientHeader.blade.php');
   }
 
   $client_id = $_GET['client_id'];
-  $sql2 = "
+  $sql = "
            SELECT
                deadline,user_name, course,remaining,description,image_name
            FROM
@@ -23,14 +23,14 @@ include('./account/partials/ClientHeader.blade.php');
              ASC
            ";
 
-    $rec2 = mysqli_query($connect, $sql2);
+    $rec = mysqli_query($connect, $sql);
 
-    if($rec2==TRUE)
+    if($rec == TRUE)
     {
-      $count = mysqli_num_rows($rec2);
+      $count = mysqli_num_rows($rec);
       if($count>0)
       {
-        while($rows=mysqli_fetch_assoc($rec2))
+        while($rows=mysqli_fetch_assoc($rec))
         {
           $image_name  = $rows['image_name'];
           $course      = $rows['course'];
@@ -39,6 +39,30 @@ include('./account/partials/ClientHeader.blade.php');
         }
       }
     }
+  $sql2 = "
+           SELECT
+               remaining - COUNT(tbl_form.lesson_id)
+           FROM
+               tbl_form
+               LEFT JOIN tbl_lesson
+                 ON tbl_form.lesson_id = tbl_lesson.lesson_id
+           GROUP BY
+               tbl_form.lesson_id
+           ";
+
+  $rec2 = mysqli_query($connect, $sql2);
+
+  if($rec2==TRUE)
+  {
+    $count = mysqli_num_rows($rec2);
+    if($count>0)
+    {
+      while($rows=mysqli_fetch_assoc($rec2))
+      {
+        $rest  = $rows['remaining - COUNT(tbl_form.lesson_id)'];
+      }
+    }
+  }
 ?>
 
 <html>
@@ -51,8 +75,9 @@ include('./account/partials/ClientHeader.blade.php');
   <div style="margin: 0 100px 0 100px">
     <div>
       <h1 style="padding: 20px ; text-align:center">Upcoming Lessons</h1>
+      <?php var_dump($rest) ?>
       <div class="cardoutline" style="display: flex;">
-        <a href="./client/form/ReserveForm.php?client_id=<?= $client_id?>" style="text-decoration: none; color: black">
+        <a href="./client/form/ReserveForm.php?client_id=<?= $client_id?>" style="text-decoration: none; color: black; margin: 13px 0">
           <div class="cardcontent" style="margin: 0 10px;">
             <span style="display: flex">
               <img src="../images/profile/<?php echo $image_name; ?>" class="c_img_index">
@@ -73,30 +98,3 @@ include('./account/partials/ClientHeader.blade.php');
   </body>
 </html>
 <?php include "./account/partials/ClientFooter.tpl"?>
-
-
-{{--SELECT--}}
-{{--deadline,user_name, course--}}
-{{--FROM--}}
-{{--tbl_lesson--}}
-{{--LEFT JOIN tbl_account--}}
-{{--ON tbl_lesson.account_id = tbl_account.account_id--}}
-{{--right JOIN tbl_form--}}
-{{--ON tbl_form.lesson_id = tbl_lesson.lesson_id--}}
-{{--WHERE--}}
-{{--NOW() > DATE_SUB(deadline, INTERVAL '13' DAY)--}}
-{{--GROUP BY--}}
-{{--tbl_form.lesson_id--}}
-
-{{--SELECT--}}
-{{--remaining - COUNT(tbl_form.lesson_id)--}}
-{{--FROM--}}
-{{--tbl_form--}}
-{{--LEFT JOIN tbl_lesson--}}
-{{--ON tbl_form.lesson_id = tbl_lesson.lesson_id--}}
-{{--GROUP BY--}}
-{{--tbl_form.lesson_id--}}
-
-{{--ORDER BY--}}
-{{--tbl_lesson.created_at--}}
-{{--ASC--}}
