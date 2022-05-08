e933d66116ed6640362b3a34139875384e495ed1
zigzagdev <msts.oo0131@gmail.com>
2022-05-08 18:10:18 +0900

    LIndex_cardcss_fix

--- LIndex.blade.php
+++ LIndex.blade.php
@@ -51,10 +51,30 @@ include('./account/partials/ClientHeader.blade.php');
   <div style="margin: 0 100px 0 100px">
     <div>
       <h1 style="padding: 20px ; text-align:center">Upcoming Lessons</h1>
-      <div class="cardoutline">
-        <div class="cardcontent">
-          <img src="../images/profile/<?php echo $image_name; ?>" class="c_img_index">
-          <p style="padding-top: 5px"><?php echo $course ?></p><br/>
+      <div class="cardoutline" style="display: flex;">
+        <div class="cardcontent" style="margin: 0 10px">
+          <span style="display: flex">
+            <img src="../images/profile/<?php echo $image_name; ?>" class="c_img_index">
+            <p style="padding-top: 5px"><?php echo $course ?></p><br/>
+          </span>
+          <?php echo $description ?><br/>
+          <?php echo $deadline ?><br/>
+          <a href="./client/form/ReserveForm.php?client_id=<?= $client_id?>"> Reserve your form</a>
+        </div>
+        <div class="cardcontent" style="margin: 0 10px">
+          <span style="display: flex">
+            <img src="../images/profile/<?php echo $image_name; ?>" class="c_img_index">
+            <p style="padding-top: 5px"><?php echo $course ?></p><br/>
+          </span>
+          <?php echo $description ?><br/>
+          <?php echo $deadline ?><br/>
+          <a href="./client/form/ReserveForm.php?client_id=<?= $client_id?>"> Reserve your form</a>
+        </div>
+        <div class="cardcontent" style="margin: 0 10px">
+          <span style="display: flex">
+            <img src="../images/profile/<?php echo $image_name; ?>" class="c_img_index">
+            <p style="padding-top: 5px"><?php echo $course ?></p><br/>
+          </span>
           <?php echo $description ?><br/>
           <?php echo $deadline ?><br/>
           <a href="./client/form/ReserveForm.php?client_id=<?= $client_id?>"> Reserve your form</a>
