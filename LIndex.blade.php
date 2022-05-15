e4c0342175140c86d6276922a44acd38dab4eeb1
zigzagdev <msts.oo0131@gmail.com>
2022-05-15 20:48:28 +0900

    fix_LIndex_padding

--- LIndex.blade.php
+++ LIndex.blade.php
@@ -108,7 +108,7 @@ include('./account/partials/ClientHeader.blade.php');
             </div>
             <div style="margin: 50px 20px 20px 20px; text-align: center; margin-top: auto">
               <strong style="float: left;">Rest Reservations</strong><br>
-              <strong>Only remain <?php var_dump($key['remaining - COUNT(tbl_form.lesson_id)']); ?> seats</strong>
+              <strong style="float: left">Only remain <?php var_dump($key['remaining - COUNT(tbl_form.lesson_id)']); ?> seats</strong>
             </div>
           </div>
 <?php } ?>
