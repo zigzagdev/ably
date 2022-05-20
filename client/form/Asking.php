97de7201431be44f1fcd30a553a77208dbc8a058
zigzagdev <msts.oo0131@gmail.com>
2022-05-21 00:25:33 +0900

    asking_form_fix

--- client/form/Asking.php
+++ client/form/Asking.php
@@ -7,6 +7,26 @@ include('../partials/FormHeader.blade.php');
     unset($_SESSION['asking_f']);
   }
   $client_id = $_GET['client_id'];
+  $lesson_id = $_GET['lesson_id'];
+
+  $sql = "
+           SELECT
+               client_id, lesson_id
+           FROM
+               tbl_form
+           WHERE 
+               client_id='$client_id' AND lesson_id='$lesson_id'
+          ";
+
+  $rec  = mysqli_query($connect,$sql);
+  $rec2 = mysqli_num_rows($rec);
+  if ($rec2 > 0) {
+    $_SESSION['asking_f'] =  "<div class='success'>You already register this course.</div>";
+    header("Location:http://localhost:8001/client/form/Asking.php?client_id=$client_id&lesson_id=$lesson_id");
+    die();
+  }
+
+
   if(isset($_POST['submit']))
   {
     $asking = $_POST['asking'];
