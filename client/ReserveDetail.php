6ae7e1339b88ee5dd317feebea41b35a41970d85
zigzagdev <msts.oo0131@gmail.com>
2022-05-21 19:28:14 +0900

    fix_ReservePHPPage

--- client/ReserveDetail.php
+++ client/ReserveDetail.php
@@ -1,10 +1,26 @@
 <?php
 include './partials/HeaderEd.blade.php';
+include "../config/Constants.blade.php";
+
+if(isset($_SESSION['asking_f']))
+{
+  echo  $_SESSION['asking_f'];
+  unset($_SESSION['asking_f']);
+}
 
 $client_id = $_GET['client_id'];
 $lesson_id = $_GET['lesson_id'];
 
-$sql = "SELECT * FROM tbl_lesson WHERE lesson_id= '$lesson_id'";
+$sql = "
+        SELECT
+            client_id, lesson_id
+        FROM
+            tbl_form
+            WHERE 
+               client_id='$client_id' 
+             AND 
+               lesson_id='$lesson_id'
+        ";
 
 $rec = mysqli_query($connect, $sql);
 
@@ -13,14 +29,9 @@ if($rec == TRUE)
   $count = mysqli_num_rows($rec);
   if ($count >= 0)
   {
-    while ($rows = mysqli_fetch_array($rec))
-    {
-      $course      = $rows['course'];
-      $description = $rows['description'];
-      $deadline    = $rows['deadline'];
-      $account_id  = $rows['account_id'];
-      $created_at  = $rows['created_at'];
-    }
+    $_SESSION['asking_f'] =  "<div class='success'>You already register this course.</div>";
+    header("Location:http://localhost:8001/LIndex.blade.php?client_id=$client_id");
+    die();
   }
 }
 
