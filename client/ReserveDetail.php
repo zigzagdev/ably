c851de6e5c8b60fd4b1d4bd6acefe4753cd9fb83
zigzagdev <msts.oo0131@gmail.com>
2022-05-22 00:45:05 +0900

    fix_ReserveDetail_btn

--- client/ReserveDetail.php
+++ client/ReserveDetail.php
@@ -45,10 +45,10 @@ if($rec == TRUE) {
     <link rel="stylesheet" href="../../css/Forms.css">
   </head>
   <body style="background: linear-gradient(90deg, whitesmoke 5%, floralwhite 55%, snow 40%, snow 100%);">
-    <div style="margin: 40px 130px 70px 130px">
-      <div class="mainaccount"style="background: linear-gradient(90deg, lightblue 5%, floralwhite 55%, ghostwhite 40%);" >
+    <div style="margin: 40px 130px 80px 130px">
+      <div class="mainaccount"style="background: linear-gradient(180deg, lightblue 5%, floralwhite 55%, ghostwhite 40%);padding-bottom: 10px" >
         <li style="list-style: none;  margin:27px 0 7px 0; padding-top: 20px; text-align: center">
-          <b style="font-size: 20px;width:70px; vertical-align: 70%; ">Your select Course.</b>
+          <b style="font-size: 20px;width:70px; vertical-align: 70%; ">Your selected Course.</b>
         </li>
         <li style="list-style: none;  margin:27px 0 17px 30px">
           <b style="font-size: 20px;margin-right:160px; float: left;">
@@ -81,7 +81,23 @@ if($rec == TRUE) {
           </b><br><br>
           <b style="font-size: 20px;overflow-wrap: break-word;"><?php echo $content ?></b>
         </li>
-        <hr color="#a9a9a9" width="100%" size="1" style="text-align: center;">
+      </div>
+      <div style="margin:60px 0; text-align: center">
+        <div style="margin: 0 10px 20px 10px">
+          <a class="btn-primary" style="margin: 0 7px 0 7px" href="UpdateLesson.blade.php?lesson_id=<?=$lesson_id=$_GET['lesson_id'];?>">
+            Update your Lesson
+          </a>
+          <a class="btn-delete" style="margin: 0 7px 0 7px;" href="DeleteLesson.php?lesson_id=<?= $lesson_id=$_GET['lesson_id'];?>">
+            Delete your Lesson
+          </a>
+<?php
+$hostname = $_SERVER['HTTP_HOST'];
+  if (!empty($_SERVER['HTTP_REFERER']) && (strpos($_SERVER['HTTP_REFERER'],$hostname) !== false))
+  {
+    echo '<a href="'. $_SERVER['HTTP_REFERER']. '" class="btn-primary" style="margin-left: 10px">Return</a>';
+  }
+?>
+        </div>
       </div>
     </div>
   </body>
