f724ed999e568c2cb68ec8bfe8cb80af90bf4d7f
zigzagdev <msts.oo0131@gmail.com>
2022-05-25 21:10:04 +0900

    fix_Update_detail

--- client/form/UpAsking.php
+++ client/form/UpAsking.php
@@ -34,27 +34,22 @@ if ($_GET['client_id'] && $_GET['form_id'])
     <link rel="stylesheet" href="../../css/Account.css">
     <link rel="stylesheet" href="../../css/Forms.css">
   </head>
-  <body>
-    <section class="food-search">
-      <div class="container2">
-        <h2 style="text-align: center">Change Your Form</h2>
-        <form action="" method="POST" class="order" style="text-align: center">
-          <fieldset class="mainaccount" style="margin: 0 310px">
-            <legend>Change Your Form</legend>
-            <p style="font-size: 20px; margin-bottom: 30px">You can change your content at below.</p>
-            <li style="list-style: none;  margin:17px 0 17px 140px">
-              <b style="font-size: 20px;width:80px;margin-right:50px; float: left;">
-                Asking
-              </b>
-              <textarea id="asking" type="text" name="asking" cols="100" rows="6"
-                        style="margin:15px 0 0 50px" required placeholder="Write something .."></textarea>
-            </li>
-          </fieldset>
-          <input type="submit" name="submit" value="送信" class="btn btn-third" style="margin-top: 41px; width: 110px; height: 54px">
-          <button type="button" onclick=history.back() class="btn-secondary" style="height: 53px; width: 103px;">Return</button>
-        </form>
-      </div>
-    </section>
+  <body style="background: linear-gradient(180deg, whitesmoke 5%, floralwhite 60%, snow 40%, snow 100%);">
+    <div style="margin: 10px 130px">
+      <strong style="text-align: left; margin: 35px 0 30px 30px;display: inline-block">Asking questions to tutor at here !!</strong>
+      <form action="" method="post">
+        <li style="list-style: none;">
+          <textarea id="asking" type="text" name="asking" cols="100" rows="6"
+                    style="margin:15px 0 0 50px" required placeholder="Write something .."></textarea>
+        </li>
+        <div style="margin:60px 0; text-align: center">
+          <div style="margin: 0 10px 20px 10px">
+            <input type="submit" name="submit" value="Submit" class="btn btn-primary" style="margin-top: 41px; width: 110px; height: 54px">
+            <button type="button" onclick=history.back() class="btn-primary" style="height: 53px; width: 103px;">Return</button>
+          </div>
+        </div>
+      </form>
+    </div>
   </body>
 </html>
 
@@ -71,12 +66,12 @@ if(isset($_POST['submit'])) {
 
   if (4 > mb_strlen($asking, 'UTF-8') || 100 < mb_strlen($asking, 'UTF-8')) {
     $_SESSION['asking_up_f'] = "<div class='success'>Please fill your content in 4~50 words. !</div>";
-    header("http://localhost:8001/client/form/UpAsking.php?client_id=$client_id&form_id=$form_id", 401);
+    header("Location:http://localhost:8001/client/form/UpAsking.php?client_id=$client_id&form_id=$form_id", 401);
     die();
   }
   if (!preg_match("/^[a-zA-Z-' ]*$/", $asking)) {
     $_SESSION['asking_up_f'] = "<div class='success'>Only English is valid !</div>";
-    header("Location:Location:http://localhost:8001/client/form/UpAsking.php?client_id=$client_id&form_id=$form_id", 401);
+    header("Location:http://localhost:8001/client/form/UpAsking.php?client_id=$client_id&form_id=$form_id", 401);
     die();
   }
 
@@ -92,8 +87,8 @@ if(isset($_POST['submit'])) {
   $rec3 = mysqli_query($connect, $sql2);
 
   if ($rec3 == true) {
-    $_SESSION['asking_up_f'] = "<div class='success'>Your asking form was updated correctly!</div>";
-    header("location: http://localhost:8001/client/ClientPage.php?client_id=$client_id");
+    $_SESSION['asking_up_suc'] = "<div class='success'>Your asking form was updated correctly !</div>";
+    header("location: http://localhost:8001/client/ClientPage.php?client_id=$client_id ");
     exit();
   } else {
     $_SESSION['asking_up_f'] = "<div class='error'>Failed to Register your form.</div>";
