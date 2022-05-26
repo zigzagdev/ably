1959f9a50d0bf88a928019469220f902c6fa0507
zigzagdev <msts.oo0131@gmail.com>
2022-05-26 23:30:02 +0900

    create_deletepage

--- client/form/DeedDelete.blade.php
+++ client/form/DeedDelete.blade.php
@@ -1,1 +1,13 @@
 <?php
+include "../partials/FormHeader.blade.php";
+
+$form_id = $_GET['form_id'];
+
+?>
+
+<html>
+<head>
+  <title>DeleteReservation</title>
+  <link rel="stylesheet" href="../../css/Account.css">
+  <link rel="stylesheet" href="../../css/Forms.css">
+</head>
