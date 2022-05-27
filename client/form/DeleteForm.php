775ef8a06d39be6d8c4dfa6cc4eed260e56258c1
zigzagdev <msts.oo0131@gmail.com>
2022-05-28 00:37:53 +0900

    add_HTML_PHP

--- client/form/DeleteForm.php
+++ client/form/DeleteForm.php
@@ -37,7 +37,8 @@ $rec = mysqli_query($connect, $sql);
   </head>
   <body>
     <div style="margin: 10px 130px">
-      <strong style="text-align: left; margin: 35px 0 30px 30px;display: inline-block">Delete Form confirm</strong>
+      <strong style="text-align: left; margin: 35px 0 30px 30px;display: inline-block">Delete Form confirm</strong><br>
+      <strong style="text-align: left; margin: 35px 0 30px 30px;display: inline-block">Are you Sure ? </strong>
     </div>
   </body>
 </html>
