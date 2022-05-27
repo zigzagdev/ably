c587be48b7cb074fb27f4670e099b99bf1a87987
zigzagdev <msts.oo0131@gmail.com>
2022-05-28 00:33:36 +0900

    fix_header

--- client/form/DeedDelete.blade.php
+++ client/form/DeedDelete.blade.php
@@ -6,10 +6,10 @@ include "../partials/FormHeader.blade.php";
 
 if($rec2 == TRUE) {
   $_SESSION['delete_lesson'] = "<div class='success'>Delete Lesson Successfully.</div>";
-  header("Location:http://localhost:8001/lesson/ManageLesson.php?account_id=$account_id", 302);
+  header("", 302);
 } else {
   $_SESSION['delete_f_lesson'] = "<div class='error'>Failed to Delete lesson.</div>";
-  header("Location:http://localhost:8001/lesson/DeleteLesson.php?id=$account_id", 401);
+  header("", 401);
 }
 
 ?>
