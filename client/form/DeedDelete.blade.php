f792c08c6851fb01b8777218c7435f5d9915d1d7
zigzagdev <msts.oo0131@gmail.com>
2022-05-29 01:31:51 +0900

    add_deleteform

--- client/form/DeedDelete.blade.php
+++ client/form/DeedDelete.blade.php
@@ -1,16 +1,65 @@
 <?php
+include "../config/Constants.blade.php";
+
+  $lesson_id = $_GET['lesson_id'];
+  $sql = "SELECT * FROM tbl_lesson WHERE lesson_id=$lesson_id";
+
+  $rec = mysqli_query($connect, $sql);
+
+if($rec == TRUE)
+{
+  $count = mysqli_num_rows($rec);
+  if ($count >= 0)
+  {
+    while ($rows = mysqli_fetch_array($rec))
+    {
+      $account_id  = $rows['account_id'];
+    }
+  }
+}
+//  $sql2= "DELETE FROM tbl_lesson WHERE lesson_id=$lesson_id";
+//  $rec2= mysqli_query($connect, $sql2);
+
+  if($rec2 == TRUE) {
+    $_SESSION['delete_lesson'] = "<div class='success'>Delete Lesson Successfully.</div>";
+    header("Location:http://localhost:8001/lesson/ManageLesson.php?account_id=$account_id", 302);
+  } else {
+    $_SESSION['delete_f_lesson'] = "<div class='error'>Failed to Delete lesson.</div>";
+    header("Location:http://localhost:8001/lesson/DeleteLesson.php?id=$account_id", 401);
+  }
+
+?><?php
 include "../partials/FormHeader.blade.php";
 
+<?php
+include "../config/Constants.blade.php";
 
+  $lesson_id = $_GET['lesson_id'];
+  $sql = "SELECT * FROM tbl_lesson WHERE lesson_id=$lesson_id";
 
+  $rec = mysqli_query($connect, $sql);
 
-if($rec2 == TRUE) {
-  $_SESSION['delete_lesson'] = "<div class='success'>Delete Lesson Successfully.</div>";
-  header("", 302);
-} else {
-  $_SESSION['delete_f_lesson'] = "<div class='error'>Failed to Delete lesson.</div>";
-  header("", 401);
+if($rec == TRUE)
+{
+  $count = mysqli_num_rows($rec);
+  if ($count >= 0)
+  {
+    while ($rows = mysqli_fetch_array($rec))
+    {
+      $account_id  = $rows['account_id'];
+    }
+  }
 }
+  $sql2= "DELETE FROM tbl_lesson WHERE lesson_id=$lesson_id";
+  $rec2= mysqli_query($connect, $sql2);
+
+  if($rec2 == TRUE) {
+    $_SESSION['delete_lesson'] = "<div class='success'>Delete Lesson Successfully.</div>";
+    header("Location:http://localhost:8001/lesson/ManageLesson.php?account_id=$account_id", 302);
+  } else {
+    $_SESSION['delete_f_lesson'] = "<div class='error'>Failed to Delete lesson.</div>";
+    header("Location:http://localhost:8001/lesson/DeleteLesson.php?id=$account_id", 401);
+  }
 
 ?>
 
