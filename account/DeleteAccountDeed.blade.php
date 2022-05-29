79d085d9b48e4c1c88d8f394614b404327839520
zigzagdev <msts.oo0131@gmail.com>
2022-05-29 22:00:38 +0900

    account_delete_complete

--- account/DeleteAccountDeed.blade.php
+++ account/DeleteAccountDeed.blade.php
@@ -1,1 +1,20 @@
 <?php
+include "../config/Constants.blade.php";
+
+$account_id= $_GET['account_id'];
+
+$sql2= "DELETE FROM tbl_account WHERE account_id=$account_id";
+$rec2= mysqli_query($connect, $sql2);
+
+if($rec2 == TRUE)
+{
+  $_SESSION['delete_ac'] = "<div class='success'>Delete Account Successfully.</div>";
+  header("Location:http://localhost:8001/Index.php", 302);
+  exit();
+} else
+{
+  $_SESSION['admin_failed'] = "<div class='error'>Failed to Add Admin.</div>";
+  header("Location:http://localhost:8001/account/DeleteAccount.blade.php?id=$account_id", 401);
+  die();
+}
+?>
